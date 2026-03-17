9402bff5a6a4126b4b1e10893283a3c3f5eeed45
Mickey <abelidasmickey134@gmail.com>
2026-03-18 07:25:01 +0800

    JavaSwing and major feature for mainclass

---
+++ src/billingsystemoop/HomePage.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package billingsystemoop;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author Mickey
+ */
+class HomePage extends JFrame implements ActionListener {
+    
+    HomePage (){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //
+    }
+    
+}
